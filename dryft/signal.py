acb9d6c3aebe0c8f3ba1912a25f51f66b1231143
Isaac Loegering <isaac.loegering@brooksrunning.com>
2026-04-28 16:27:56 -0700

    Modified detrend() function to not extrapolate with a cubic spline but rather to backfill and forward fill. Added 'method' parameter to aerialforce() to enable calculation of aerial force as either the mid-swing point force or the median swing force. Added 'verbose' parameter to splitsteps to control console outputs. Added splitsteps_dynamic_threshold() function that optimizes the force threshold for step event detection used for drift correction by iteratively lowering the threhold from the median force till the number of events increases (as happens when the threshold drops below the level of noise).

--- dryft/signal.py
+++ dryft/signal.py
@@ -46,7 +46,7 @@ def detrend(force_f, aerial, aerial_loc):
     drift_signal[aerial_loc] = aerial
     # Use 3rd order spline to fill NaNs, creating the underlying drift of the signal.
     drift_signal_p = pd.Series(drift_signal)
-    drift_signal_p = drift_signal_p.interpolate(method = 'spline', order = 3, s = 0, limit_direction= 'neither')
+    drift_signal_p = drift_signal_p.interpolate(method = 'spline', order = 3, s = 0, limit_direction= "both", limit_area="inside")
     drift_signal_p = drift_signal_p.ffill().bfill()
     drift_signal = drift_signal_p.to_numpy()
     # Subtract drift from force signal
@@ -109,7 +109,7 @@ def findgoodaerial(stance_begin, stance_end, good_stances):
     return good_aerial_begin, good_aerial_end
 
 
-def aerialforce(force, stance_begin, stance_end, good_stances):
+def aerialforce(force, stance_begin, stance_end, good_stances, method="middle"):
     """Calculate force signal at middle of aerial phase of running.
 
     Parameters
@@ -122,11 +122,13 @@ def aerialforce(force, stance_begin, stance_end, good_stances):
         Array of frame indexes for end of each stance phase. Same size as `stance_begin`.
     good_stances : `ndarray`
         Boolean array of which stance phases meet min_tc & max_tc requirements.
+    method: `str`
+        Method for calculating aerial phase force. Options are "middle" (force at middle of aerial phase) or "median" (median force across aerial phase).
 
     Returns
     -------
     aerial : `ndarray`
-        Array containing force measured at middle of aerial phase force signal.
+        Array containing aerial phase force.
     aerial_loc : `ndarray`
         Array of frame indexes for values in aerial. output from `aerialforce()`
 
@@ -138,25 +140,29 @@ def aerialforce(force, stance_begin, stance_end, good_stances):
         aerial_end = stance_begin[good_stances][1:]
 
     # calculate force at middle of aerial phase (foot not on ground, should be zero)
-    if force.ndim == 2:  # one axis only
-        force = force.flatten()
-        aerial = np.full([aerial_begin.shape[0], ], np.nan)
-        aerial_middle = np.full([aerial_begin.shape[0], ], np.nan)
-    elif force.ndim == 1:
-        aerial = np.full([aerial_begin.shape[0], ], np.nan)
-        aerial_middle = np.full([aerial_begin.shape[0], ], np.nan)
-    else: raise IndexError('Can only handle shape (n,) or (n,1)')
+    if force.ndim > 2:
+        raise IndexError('Can only handle shape (n,) or (n,m)')
+    # if force.ndim == 2:  # one axis only
+    #     force = force.flatten()
+    aerial = np.full([aerial_begin.shape[0], force.shape[1]], np.nan)
+    aerial_middle = np.full([aerial_begin.shape[0], ], np.nan)
 
+    aerial_len = aerial_end-aerial_begin
     for i in range(aerial.shape[0]):
-        aerial_len = aerial_end-aerial_begin
         aerial_middle[i,] = round(aerial_len[i]/2)
-        aerial[i,] = force[aerial_begin[i]+aerial_middle.astype(int)[i]]
+        match method:
+            case "middle":
+                aerial[i,:] = force[aerial_begin[i]+aerial_middle[i].astype(int), :]
+            case "median":
+                aerial[i,:] = np.median(force[aerial_begin[i]:aerial_end[i]], axis=0)
+            case _:
+                raise ValueError('Method not recognized. Please choose "middle" or "median".')
     aerial_loc = aerial_begin + aerial_middle.astype(int)
 
     return aerial, aerial_loc
 
 
-def splitsteps(vGRF, threshold, Fs, min_tc, max_tc, plot=False):
+def splitsteps(vGRF, threshold, Fs, min_tc, max_tc, verbose=True, plot=False):
     """Read in filtered vertical ground reaction force (vGRF) signal and ID stance phases based on a threshold.
 
     Designed for running, hopping, or activity where 1 foot is on the force plate at a time.
@@ -177,6 +183,8 @@ def splitsteps(vGRF, threshold, Fs, min_tc, max_tc, plot=False):
         Minimum contact time, in seconds, to consider as a real stance phase. Jogging > 0.2s
     max_tc : number
         Maximum contact time, in seconds, to consider as a real stance phase. Jogging > 0.4s
+    verbose : `bool`
+        If true, print information about the stance phases identified.
     plot : `bool`
         If true, return plot showing vGRF signal with initial and final contact points identified. Helpful for
         determining appropriate threshold, min_tc, and max_tc values.
@@ -213,6 +221,7 @@ def splitsteps(vGRF, threshold, Fs, min_tc, max_tc, plot=False):
         stance_end_all = np.squeeze(np.asarray(np.nonzero(events == -1)).transpose())
 
         if plot:
+            plt.figure()
             plt.plot(vGRF)
             plt.plot(events*10)
             plt.show(block = False)
@@ -225,17 +234,61 @@ def splitsteps(vGRF, threshold, Fs, min_tc, max_tc, plot=False):
         stance_len = stance_end_all - stance_begin_all
         good_stances = np.logical_and(stance_len >= min_tc*Fs, stance_len <= max_tc*Fs)
 
-        # ID suspicious stance phases (too long or short)
-        if np.any(stance_len < min_tc*Fs):
-            print('Out of', stance_len.shape[0], 'stance phases,', sum(stance_len < min_tc*Fs), ' < ',
-                  min_tc, 'seconds.')
-        if np.any(stance_len > max_tc*Fs):
-            print('Out of', stance_len.shape[0], 'stance phases,', sum(stance_len > max_tc*Fs), ' > ',
-                  max_tc, 'seconds.')
-        # print sizes
-        print('Total number of contact time begin/end:', stance_begin_all.shape[0], stance_end_all.shape[0])
+        if verbose:
+            # ID suspicious stance phases (too long or short)
+            if np.any(stance_len < min_tc*Fs):
+                print('Out of', stance_len.shape[0], 'stance phases,', sum(stance_len < min_tc*Fs), ' < ',
+                    min_tc, 'seconds.')
+            if np.any(stance_len > max_tc*Fs):
+                print('Out of', stance_len.shape[0], 'stance phases,', sum(stance_len > max_tc*Fs), ' > ',
+                    max_tc, 'seconds.')
+            # print sizes
+            print('Total number of contact time begin/end:', stance_begin_all.shape[0], stance_end_all.shape[0])
 
         return stance_begin_all, stance_end_all, good_stances
 
     else:
         raise IndexError('Did not ID stance phases: min_tc > max_tc.')
+
+def splitsteps_dynamic_threshold(vGRF, threshold_quantile=0.5, Fs=2000, min_tc=0.14, max_tc=0.4, quantile_step=0.001, target_events=None, plot=False):
+    # Calculate threshold
+    threshold = np.quantile(vGRF, threshold_quantile)
+    # Perform step event detection
+    step_begin_all, step_end_all, good_steps = splitsteps(
+        vGRF=vGRF,
+        threshold=threshold,
+        Fs=Fs,
+        min_tc=min_tc,
+        max_tc=max_tc,
+        verbose=False,
+    )
+    # Count events with current threshold
+    num_events = len(step_begin_all) + len(step_end_all)
+    # Not first iteration and new event detected compared to previous iteration
+    if (target_events is not None) and (num_events > target_events):
+        error_message = f"The number events detected during threshold optimization increased from {target_events} to {num_events} when threshold was decreased from {np.quantile(vGRF, threshold_quantile + quantile_step)} to {threshold}. This may indicate that the threshold is too low and is detecting noise as events."
+        raise ThresholdError(error_message)
+    # First iteration or no increase in events, continue optimizing threshold
+    try:
+        return splitsteps_dynamic_threshold(vGRF, threshold_quantile=threshold_quantile - quantile_step, Fs=Fs, min_tc=min_tc, max_tc=max_tc, quantile_step=quantile_step, target_events=num_events, plot=plot)
+    # Optimized threshold found
+    except ThresholdError:
+        precision = np.quantile(vGRF, threshold_quantile + quantile_step) - threshold
+        print(f"Optimal threshold: {threshold:.2f} (within {precision:.2f}) N, with {len(step_begin_all)} steps detected.")
+        if plot:
+            events = np.zeros_like(vGRF)
+            events[step_begin_all] = 1
+            events[step_end_all] = -1
+            plt.figure()
+            plt.plot(vGRF, label='vGRF')
+            plt.axhline(y=threshold, color='b', linestyle='--', label='Threshold')
+            plt.scatter(step_begin_all, vGRF[step_begin_all], color='g', label='Step Begin')
+            plt.scatter(step_end_all, vGRF[step_end_all], color='r', label='Step End')
+            plt.legend()
+            plt.show(block = False)
+        return step_begin_all, step_end_all, good_steps
+
+class ThresholdError(Exception):
+    """Custom exception for threshold optimization errors."""
+    def __init__(self, message=None):
+        super().__init__(message)
